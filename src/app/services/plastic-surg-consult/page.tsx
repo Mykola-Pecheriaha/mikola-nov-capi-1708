4ca64b5e6f459ae4749e66015b3a6c630f072fd4
Mykola-Pecheriaha <06072025git@gmail.com>
2025-08-24 16:32:12 +0300

    Services

--- src/app/services/plastic-surg-consult/page.tsx
+++ src/app/services/plastic-surg-consult/page.tsx
@@ -1,5 +1,9 @@
 import PlasticSurgConsult from "@/components/PlasticSurgConsult/PlasticSurgConsult";
 
 export default function PlasticSurgConsultPage() {
-  return <PlasticSurgConsult />;
+  return (
+    <div className="container mx-auto px-4">
+      <PlasticSurgConsult />
+    </div>
+  );
 }
